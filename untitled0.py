9ffa88691188a553ae3800186a722e37a00bdc13
Kaihong Huang <kaihong.huang11@gmail.com>
2016-06-02 15:11:36 +0200

    [ok] 3d plot added

--- untitled0.py
+++ untitled0.py
@@ -15,6 +15,7 @@ def sim(*arg,**kwarg):
     return np.hstack(arg)
 import scipy
 import scipy.ndimage
+from vtk_visualizer import *
 
 if __name__ == "__main__":
     imleft = plt.imread('./left0000.jpg')[:,:,0].astype('f').copy()
@@ -54,11 +55,11 @@ if __name__ == "__main__":
         data[y][c].append(x)
 
  #%%
+    x_off, y_off = np.meshgrid(range(-2,3),range(-2,3))
     def calcPatchScore(y, x0, x_can):
-        x_off, y_off = np.meshgrid(range(-2,3),range(-2,3))
         dl =  imleft[y+y_off,    x0+x_off]
         dr = imright[y+y_off, x_can+x_off]
-#        ncc = np.dot(normalize(dl.ravel()-dl.mean()),
+#        ncc = 1+np.dot(normalize(dl.ravel()-dl.mean()),
 #                     normalize(dr.ravel()-dr.mean()))
         ssd = np.linalg.norm((dl-dr).ravel())/25.0
         return ssd
@@ -82,52 +83,64 @@ if __name__ == "__main__":
     b0.imshow(imleft)
 
 #    x,y,c = px[0][1000], py[0][1000], pcolor[0][1000]
-    start = 1000
+
+    start = 0
+    debug = False
+    d_result = np.full_like(imleft, np.inf)
     for x,y,c in zip(px[0][start:], py[0][start:], pcolor[0][start:]):
-        pf(fi.number)
-        b0.plot(x,y,'r.')
-        pf(f.number)
-        a0.clear()
-        a1.clear()
-        '''show the data slice'''
-        a0.plot(dI[0][y,:],'r')
-        a0.plot(dI[1][y,:],'b')
-        ''' plot target point'''
-        a0.vlines(x,0,scale,'r')
-        a1.plot( imleft[y,:],'r')
-        a1.plot(imright[y,:],'b')
-        a1.vlines(x,0,255,'r')
-        p_success = []
+        if debug:
+            pf(fi.number)
+            b0.plot(x,y,'r.')
+            pf(f.number)
+            a0.clear()
+            a1.clear()
+            '''show the data slice'''
+            a0.plot(dI[0][y,:],'r')
+            a0.plot(dI[1][y,:],'b')
+            ''' plot target point'''
+            a0.vlines(x,0,scale,'r')
+            a1.plot( imleft[y,:],'r')
+            a1.plot(imright[y,:],'b')
+            a1.vlines(x,0,255,'r')
+        min_score = np.inf
         '''consider all points in neighbouring gradient-level as candiates'''
         for offset in [0,1,-1,2,-2,3,-3]:
-            plist = data[y][c+offset]
+            plist = data[y][np.clip(c+offset, 0, scale)]
             for cp_x in plist:
 
                 ''' discard points in negative or out-of-range disparity'''
                 dis = x-cp_x
-                if dis < 0 or dis > 120 :   # discard points in negative  or out-of-range disparity
+                if dis < 15 or dis > 120 :   # discard points in negative  or out-of-range disparity
                     continue
 
                 ''' discard points different to much in intensity'''
                 if np.abs(imleft[y,x]-imright[y,cp_x]) > 15 :
                     continue
 
-                a0.vlines(cp_x,0,scale,'b','dashed')
-                a1.vlines(cp_x,0,255,'b','dashed')
+                if debug:
+                    a0.vlines(cp_x,0,scale,'b','dashed')
+                    a1.vlines(cp_x,0,255,'b','dashed')
 
+                ''' discard points with poor score'''
                 score = calcPatchScore(y,x,cp_x)
-#                if score >100:   # discard points with poor score
-#                    continue
-                p_success.append((score, cp_x))
+                if score > 3:
+                    continue
 
-        if p_success:
-            '''get the best match'''
-            p_d = min(p_success, key=lambda x:x[0])
-            print 'depth at %d, c:%f' % (p_d[1],p_d[0])
-            a0.vlines(p_d[1],0,scale,'g',linewidths=3)
-            a1.vlines(p_d[1],0,255,'g',linewidths=3)
-        else:
-            print 'failed to find a match'
+                if score < min_score:   # only keep the best match
+                    min_score = score
+                    d_result[y,x] = x-cp_x
 
-        plt.pause(0.01)
-        plt.waitforbuttonpress()
+        if debug:
+            print 'depth at %d with score:%f' % (cp_x, min_score)
+            a0.vlines(x-d_result[y,x],0,scale,'g',linewidths=3)
+            a1.vlines(x-d_result[y,x],0,255,'g',linewidths=3)
+
+            plt.pause(0.01)
+            plt.waitforbuttonpress()
+
+    v,u = np.where(d_result != 0)
+    p3d = np.vstack([(u-0.5*w)/435.016,
+                     (v-0.5*h)/435.016,
+                     np.ones(u.shape[0])
+                     ]).astype('f')/d_result[v,u]*0.119554
+    plotxyzrgb(np.vstack([p3d,np.tile(imleft[v,u],(3,1))]).T)
