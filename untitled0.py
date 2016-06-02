9c54067ca4c5903fcee79860e6d8fcf2291b6223
Kaihong Huang <kaihong.huang11@gmail.com>
2016-06-02 12:20:35 +0200

    [ok] using ssd score

--- untitled0.py
+++ untitled0.py
@@ -26,9 +26,9 @@ if __name__ == "__main__":
     #%% get good pixels
     dI,px,py,pcolor,pvm = [],[],[],[],[]
     for i,im in enumerate([imleft, imright]):
-        d = scipy.ndimage.filters.gaussian_laplace(im,1)
+        d = scipy.ndimage.filters.gaussian_gradient_magnitude(im,1)
         d_abs = np.abs(d)
-        valid_mask = d_abs>np.percentile(d_abs,85)
+        valid_mask = d_abs>np.percentile(d_abs,70)
         dI.append( d.copy() )
         u, v = np.meshgrid(range(w),range(h))
         pixel_mask = reduce(np.logical_and,[valid_mask, u>1, v>1, u<w-2, v<h-2])
@@ -54,25 +54,28 @@ if __name__ == "__main__":
         data[y][c].append(x)
 
  #%%
-
-
-
     def calcPatchScore(y, x0, x_can):
         x_off, y_off = np.meshgrid(range(-2,3),range(-2,3))
         dl =  imleft[y+y_off,    x0+x_off]
         dr = imright[y+y_off, x_can+x_off]
-        ncc = np.dot(normalize(dl.ravel()-dl.mean()),
-                     normalize(dr.ravel()-dr.mean()))
-        return ncc
+#        ncc = np.dot(normalize(dl.ravel()-dl.mean()),
+#                     normalize(dr.ravel()-dr.mean()))
+        ssd = np.linalg.norm((dl-dr).ravel())/25.0
+        return ssd
+    from operator import itemgetter
+
     def test_calcPatchScore():
-        x0, y = (302,180)
+        x0, y = (786, 25)
         score = [calcPatchScore(y, x0, x_can) for x_can in range(2,w-2)]
         plt.subplot(2,1,1)
         plt.plot(score)
         plt.subplot(2,1,2)
-        plt.plot( imleft[y,2:-2],'r')
-        plt.plot(imright[y,2:-2],'b')
+        plt.plot( imleft[y,:],'r')
+        plt.plot(imright[y,:],'b')
         plt.vlines(x0,0,255,'r')
+        plt.hlines(imleft[y,x0],2,w-2)
+        result, element = min(enumerate(score), key=itemgetter(1))
+        plt.vlines(result+2,0,255,'g',linewidths=3)
 
     f,(a0,a1) = plt.subplots(2,1,num='query')
     fi, b0 = plt.subplots(1,1,num='i')
@@ -90,26 +93,39 @@ if __name__ == "__main__":
         a0.plot(dI[0][y,:],'r')
         a0.plot(dI[1][y,:],'b')
         ''' plot target point'''
-        a0.vlines(x,0,256,'r')
+        a0.vlines(x,0,scale,'r')
         a1.plot( imleft[y,:],'r')
         a1.plot(imright[y,:],'b')
-        a1.vlines(x,0,256,'r')
+        a1.vlines(x,0,255,'r')
         p_success = []
         '''consider all points in neighbouring gradient-level as candiates'''
-        for offset in [0,1,-1,2,-2]:
+        for offset in [0,1,-1,2,-2,3,-3]:
             plist = data[y][c+offset]
             for cp_x in plist:
-                '''do the match'''
-                if x-cp_x < 0:   # discard point in negative disparity
+
+                ''' discard points in negative or out-of-range disparity'''
+                dis = x-cp_x
+                if dis < 0 or dis > 120 :   # discard points in negative  or out-of-range disparity
                     continue
-                p_success.append((calcPatchScore(y,x,cp_x), cp_x))
-                a0.vlines(cp_x,0,256,'b')
-                a1.vlines(cp_x,0,256,'b')
+
+                ''' discard points different to much in intensity'''
+                if np.abs(imleft[y,x]-imright[y,cp_x]) > 15 :
+                    continue
+
+                a0.vlines(cp_x,0,scale,'b','dashed')
+                a1.vlines(cp_x,0,255,'b','dashed')
+
+                score = calcPatchScore(y,x,cp_x)
+#                if score >100:   # discard points with poor score
+#                    continue
+                p_success.append((score, cp_x))
+
         if p_success:
-            p_d = max(p_success, key=lambda x:x[0])
+            '''get the best match'''
+            p_d = min(p_success, key=lambda x:x[0])
             print 'depth at %d, c:%f' % (p_d[1],p_d[0])
-            a0.vlines(p_d[1],0,256,'g')
-            a1.vlines(p_d[1],0,256,'g')
+            a0.vlines(p_d[1],0,scale,'g',linewidths=3)
+            a1.vlines(p_d[1],0,255,'g',linewidths=3)
         else:
             print 'failed to find a match'
 
