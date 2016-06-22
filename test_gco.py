235b2fd55ca7e7ffb09e41f113377d60e7b32083
Kaihong Huang <kaihong.huang11@gmail.com>
2016-06-22 22:22:24 +0200

    [!]save

--- test_gco.py
+++ test_gco.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         py.append(v[pixel_mask].copy())
         pvm.append(pixel_mask.copy())
 
-    pis(valid_mask)
+    pis(sim(valid_mask))
 
     for vm,d in zip(pvm, dI):
         dt = np.round(d).astype('int')
@@ -63,33 +63,44 @@ if __name__ == "__main__":
     d_result = np.full_like(lim, -1)
 
 #%% edge-only stereo
-
+    from scipy import sparse
+    vec = lambda x:np.reshape(x,(-1,1))
+    py, px = pvm[0].nonzero()
+    node_cnt = px.size
     '''1. setup neighbors'''
     def makeEdges(mask_image):
         py, px = mask_image.nonzero()
         node_cnt = px.size
-
         id_LUT = np.empty_like(mask_image, 'i4')
         id_LUT[mask_image] = range(node_cnt)      # lookup-table of index number for valid pixels
         edges = []
-        for e_id, e_x,e_y in zip(range(node_cnt), px, py):
-            if mask_image[e_y-1, e_x+1]:
-                edges.append([e_id, id_LUT[e_y-1, e_x+1]])
-            if mask_image[e_y-1, e_x]:
-                edges.append([e_id, id_LUT[e_y-1,  e_x ]])
-            if mask_image[e_y-1, e_x-1]:
-                edges.append([e_id, id_LUT[e_y-1, e_x-1]])
-            if mask_image[e_y, e_x-1]:
-                edges.append([e_id, id_LUT[ e_y,  e_x-1]])
-        return np.array(edges,'i4')       # edges array, each row represent an edge
-    edges = makeEdges(pvm[0])
+        for p_id, p_x,p_y in zip(range(node_cnt), px, py):
+            degree = 0
+            '''diagonal edge'''
+            if mask_image[p_y-1, p_x+1]:
+                edges.append([p_id, id_LUT[p_y-1, p_x+1]]); degree += 1
+            if mask_image[p_y-1, p_x-1]:
+                edges.append([p_id, id_LUT[p_y-1, p_x-1]]); degree += 1
+
+            if mask_image[p_y-1, p_x]:
+                edges.append([p_id, id_LUT[p_y-1,  p_x ]]); degree += 1
+            if mask_image[p_y, p_x-1]:
+                edges.append([p_id, id_LUT[ p_y,  p_x-1]]); degree += 1
+
+        edge_cnt = len(edges)
+        row_ind = np.tile(np.arange(edge_cnt)[:,np.newaxis],2).ravel()
+        col_ind = np.array(edges).ravel()
+        data = np.tile(np.array([1,-1]), edge_cnt)
+        incidence_matrix = sparse.csr_matrix((data,(row_ind,col_ind)), (len(edges),node_cnt),'i4')
+
+        return np.array(edges,'i4'), incidence_matrix      # edges array, each row represent an edge
+    edges,incidence_matrix = makeEdges(pvm[0])
 
     '''2. setup matching cost'''
     max_disp = 150
-    vec = lambda x:np.reshape(x,(-1,1))
-    py, px = pvm[0].nonzero()
 
-    unary_cost = np.empty([px.size, max_disp+1],'i4')       # shape= n_vertices x n_disps
+
+    unary_cost = np.empty([node_cnt, max_disp+1],'i4')       # shape= n_vertices x n_disps
     y_ind, x_ind = vec(py) , vec(px)-np.arange(max_disp+1)
     unary_cost = np.abs(vec(lim[py, px]) - rim[y_ind, x_ind])  # shape=(n_vertices, n_labels)
     unary_cost[x_ind<0] = 100
@@ -114,7 +125,21 @@ if __name__ == "__main__":
                      np.ones(u.shape[0])
                      ]).astype('f')/d_result[v,u]*0.119554
     plotxyzrgb(np.vstack([p3d,np.tile(lim[v,u],(3,1))]).T)
+#%%
+    def sample(x):
+        return scipy.ndimage.map_coordinates(rim, [py,px+x], order=1, mode='constant', cval=1000)
 
+    tau = 0.5
+
+    from pyunlocbox import functions,solvers
+    fsmooth = functions.norm_l2(A =lambda x: incidence_matrix.dot(x),
+                                At=lambda x: incidence_matrix.transpose().dot(x))
+    fdata = functions.norm_l1(y=lim[py,px],
+                              A=sample,
+                              lambda_= tau )
+    solver = solvers.forward_backward(method='FISTA', step=0.5/tau)
+    x0 = d_cut
+    ret = solvers.solve([fdata, fsmooth], x0, solver, maxit=100)
 #%% full-image stereo
     exit()
     max_disp = 150
