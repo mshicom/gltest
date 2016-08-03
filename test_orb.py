25941a85a2df12c8f4a464219177f289f19d686d
Kaihong Huang <kaihong.huang11@gmail.com>
2016-08-03 09:08:36 +0200

    [!]fixed calcEPL bug; changed dataset

--- test_orb.py
+++ test_orb.py
@@ -28,7 +28,14 @@ def loaddata1():
     Z, = data['Z']/100.0
     return frames, G, K, Z
 
+def loaddata2():
+    with np.load("kfrecord.npz") as data:
+        frames = data["frames"]
+        G = data["Gs"]
+        K = data['K']
+        r = data['r']
 
+    return frames, G, K
 
 def metric(P): return P[:-1]/P[-1]
 def skew(e): return np.array([[  0,  -e[2], e[1]],
@@ -50,6 +57,8 @@ def relPos(wG0, wG1):
 
 def transform(G,x):
     return G[:3,:3].dot(x)+G[:3,3]
+def transforms(G,x):
+    return G[:3,:3].dot(x)+G[:3,3][:,np.newaxis]
 
 def conditions(*args):
     return reduce(np.logical_and, args)
@@ -59,10 +68,12 @@ snormalize = lambda x:x/np.linalg.norm(x, axis=0)
 vec = lambda x:np.reshape(x,(-1,1))
 inv = np.linalg.inv
 
+
 if __name__ == "__main__":
     if 'frames' not in globals() or 1:
-        frames, wGc, K, Zs = loaddata1()
-        h,w = frames[0].shape[:2]
+#        frames, wGc, K, Zs = loaddata1()
+        frames, wGc, K = loaddata2()
+    h,w = frames[0].shape[:2]
     fx,fy,cx,cy = K[0,0],K[1,1],K[0,2],K[1,2]
 
 #%%
@@ -179,53 +190,93 @@ if __name__ == "__main__":
             return P1[:2]/P1[2]
 
         def searchEPL(self, f1, win_width=4):
-
             # xr, yr, win_width = pref[0], pref[1],4
             px, py = self.px, self.py
             node_cnt = self.p_cnt
             rGc = relPos(self.wGc, f1.wGc)
 
             pb,dm,dxy,dxy_local = calcEpl(px, py, rGc)
-            best = np.empty((2,node_cnt),'f')
+            best = np.empty(node_cnt,'i')
+            if 0:
+                for n in xrange(node_cnt):
+                    print n
+                    p0 = np.array([px[n],py[n]])
 
+                    ref_pos = vec(p0) - vec(dxy_local[:,n])*np.arange(-win_width, win_width+1) # TODO: correct the sign
+                    ref_samples = sample(self.im, ref_pos[0], ref_pos[1])
 
-            for n in xrange(node_cnt):
-                print n
-                p0 = np.array([px[n],py[n]])
+                    sam_cnt = np.floor(dm[n])
+                    cur_pos = vec(pb[:,n]) + vec(dxy[:,n])*np.arange(-win_width, sam_cnt+win_width)
+                    cur_samples = sample(f1.im, cur_pos[0], cur_pos[1])
+                    err = np.empty(sam_cnt,'f')
 
-                ref_pos = vec(p0) - vec(dxy_local[:,n])*np.arange(-win_width, win_width+1) # TODO: correct the sign
-                ref_samples = sample(self.im, ref_pos[0], ref_pos[1])
-
-                sam_cnt = np.floor(dm[n])
-                cur_pos = vec(pb[:,n]) + vec(dxy[:,n])*np.arange(-win_width, sam_cnt+win_width)
-                cur_samples = sample(f1.im, cur_pos[0], cur_pos[1])
-                err = np.empty(sam_cnt,'f')
-#                if 0:
-#                for i in xrange(int(sam_cnt)):
-#                    diff = ref_samples - cur_samples[i:i+2*win_width+1]
-#                    vm = ~np.isnan(diff)
-#                    err[i] = np.minimum(diff[vm]**2, 1**2).sum()/vm.sum()
-#                best[:,n] = cur_pos[:,np.nanargmin(err)+win_width]
-#                else:
-                code = r'''
-                for(int i=0; i<(int)sam_cnt; i++ )
+                    for i in xrange(int(sam_cnt)):
+                        diff = ref_samples - cur_samples[i:i+2*win_width+1]
+                        vm = ~np.isnan(diff)
+                        err[i] = np.minimum(diff[vm]**2, 1**2).sum()/vm.sum()
+                    best[n] = np.nanargmin(err)
+            else:
+                scode = r'''
+                inline float sample(const float* const mat, const int width, const float x, const float y)
                 {
-                    float diff = 0;
-                    for(size_t j=0; j<2*%d+1;j++ )
-                    {
-                        float cur = CUR_SAMPLES1(i+j);
-                        diff += std::isnan(cur)? 0 : std::fabs( REF_SAMPLES1(j) - cur);
+                    	int ix = (int)x;
+                    	int iy = (int)y;
+                    	float dx = x - ix;
+                    	float dy = y - iy;
+                    	float dxdy = dx*dy;
+                    	const float* bp = mat +ix+iy*width;
+                    	float res =   dxdy * bp[1+width]
+                    				+ (dy-dxdy) * bp[width]
+                    				+ (dx-dxdy) * bp[1]
+                    				+ (1-dx-dy+dxdy) * bp[0];
+                    	return res;
+                } '''
+                code = r'''
+                size_t M = node_cnt;
+                const int win_width = %(win_width)d;
+                const int win_size = 2*win_width+1;
+                std::vector<float> ref_samples(2*win_width+1);
+                std::vector<float> cur_samples(1000);
+
+                // foreach pixel in ref image
+                for(size_t p=0; p<M; p++){
+                    /* 1. Sampling the Intensity */
+                    for(int i=0; i<win_size; i++)
+                        ref_samples[i] = sample(ima,width,
+                                                PX1(p)-(i-win_width)*DXY_LOCAL2(0,p),
+                                                PY1(p)-(i-win_width)*DXY_LOCAL2(1,p));
+
+                    size_t sample_size = (size_t)DM1(p);
+                    cur_samples.reserve(sample_size+2*win_width);
+
+                    for(int i=0; i<sample_size+2*win_width; i++)
+                        cur_samples[i] = sample(imb, width,
+                                                PB2(0,p)+(i-win_width)*DXY2(0,p),
+                                                PB2(1,p)+(i-win_width)*DXY2(1,p));
+
+                    /* 2. Matching */
+                    float min_diff = std::numeric_limits<float>::max();
+                    for(int i=0; i<sample_size; i++ ){
+                        float diff = 0;
+                        for(size_t j=0; j<win_size;j++ ){
+                            float cur = cur_samples[i+j];
+                            diff += std::isnan(cur)? 0 : std::fabs(ref_samples[j] - cur);
+                        }
+                        if (diff<min_diff){
+                            min_diff = diff;
+                            BEST1(p) = i;
+                        }
                     }
-                    ERR1(i) = diff;
                 }
-                ''' % win_width
+                '''% {'win_width': win_width }
+                ima,imb,width = np.ascontiguousarray(self.im), np.ascontiguousarray(f1.im), self.im.shape[1]
                 weave.inline(code,
-                   ['ref_samples','cur_samples','err', 'sam_cnt'],
-                    compiler='gcc',headers=['<chrono>','<cmath>'],
+                   ['ima','imb','width','node_cnt','pb','dm','dxy','dxy_local','px','py','best' ],#
+                    support_code=scode, compiler='gcc',headers=['<algorithm>','<cmath>','<vector>'],
                     extra_compile_args=['-std=gnu++11 -msse2 -O3'],
                     verbose=2 )
-                best[:,n] = cur_pos[:,np.nanargmin(err)+win_width]
-            return best
+            res = pb+best*dxy
+            return res
 
         def makePC(self, depth, vmin=0, vmax=10):
             vm_ind, = np.where(conditions(depth>vmin,  depth<vmax))
@@ -233,9 +284,6 @@ if __name__ == "__main__":
             I = np.tile(self.im[self.py, self.px]*255,(3,1))
             P = np.vstack([p3d, I]).T
             return P[vm_ind,:]      # Nx6
-
-
-
 
 
     def calcF(rGc, K=K):
@@ -246,6 +294,7 @@ if __name__ == "__main__":
 
     def calcEpl(xr,yr,rGc,K=K):
         ''' pc = Pinf + depth*dxy '''
+        # xr,yr,rGc = pref[0], pref[1], getG(f0,f1)
         cGr = inv(rGc)
         Rcr,Tcr = cGr[:3,:3],cGr[:3,3]
         pr = np.array([xr, yr, 1 if isScalar(xr) else np.ones(len(xr))])
@@ -253,10 +302,11 @@ if __name__ == "__main__":
         Pinf /= Pinf[2]
         Pe = K.dot(Tcr)
 
+#        dxy = normalize(np.array([Tcr[2]*(xr-cx) -fx*Tcr[0], Tcr[2]*(yr-cy) -fy*Tcr[1]]))
         rFc = calcF(rGc)
         a,b,c = np.hsplit(pr.T.dot(rFc), 3)    # a*xc+b*yc+c=0
         norm = np.sqrt(a**2+b**2)
-        dxy = np.hstack([np.sign(Tcr[0])*np.abs(b/norm), -np.sign(Tcr[1])*np.abs(a/norm)]).T  # TODO: principle?
+        dxy = np.sign(Tcr[2])*np.sign(Tcr[0])*np.hstack([-b/norm, a/norm]).T  # TODO: principle?
 
         x_limit = np.maximum(-Pinf[0]/dxy[0], (w-Pinf[0])/dxy[0])   # Pinf.x + x_limit*dx = {0,w}
         y_limit = np.maximum(-Pinf[1]/dxy[1], (h-Pinf[1])/dxy[1])   # Pinf.y + y_limit*dy = {0,h}
@@ -266,8 +316,9 @@ if __name__ == "__main__":
         Trc = rGc[:3,3]
         a,b,c = np.hsplit(rFc.dot(Pinf).T, 3)    # a*xc+b*yc+c=0
         norm = np.sqrt(a**2+b**2)
-        dxy_local = np.hstack([np.sign(Trc[0])*np.abs(b/norm), -np.sign(Trc[1])*np.abs(a/norm)]).T  # TODO: principle?
+        dxy_local = np.sign(Trc[2])*np.sign(Trc[0])*np.hstack([-b/norm, a/norm]).T  # TODO: principle?
 
+#        dxy_local = normalize(np.array([Trc[2]*(Pinf[0]-cx)-fx*Trc[0], Trc[2]*(Pinf[0]-cy)-fy*Trc[1]]))
 #        '''P = Pinf + λ*Pe'''
 #        az = -Pinf[2]/Pe[2]                         # Pz = Pinf[2] + λ*Pe[2] > 0
 #        ax = (Pinf[0]-w*Pinf[2])/(w*Pe[2]-Pe[0])    # 0 < fx*(Px/Pz) + cx < w
@@ -286,10 +337,7 @@ if __name__ == "__main__":
 #
 #        Pe = Pe/Pe[2]
 #        Pinf = Pinf/Pinf[2]
-
         return Pinf[:2],dinv_max, dxy, dxy_local
-
-
 
     def test_calcEpl():
         f,a = plt.subplots(1, 1, num='test_F')
@@ -300,12 +348,16 @@ if __name__ == "__main__":
             a.plot(pref[0], pref[1],'r.')
 
             cGr = relPos(f1.wGc, f0.wGc)
-            pcur = trueProj(pref[0], pref[1], cGr=cGr, Zr=f0.Z)
+            Z = np.linspace(0.1, 5.0, 40)
+            pcur = K.dot(transforms(cGr, (backproject(pref[0], pref[1])*vec(Z)).T))
+            pcur /= pcur[2]
+#            pcur = trueProj(pref[0], pref[1], cGr=cGr, Zr=f0.Z)
             a.plot(pcur[0]+640, pcur[1],'b.')
 
             pb,dmax,dxy,dxy_local = calcEpl(pref[0], pref[1], inv(cGr))
+            pe = pb+dmax*dxy
             a.plot([pb[0]+640,pe[0]+640], [pb[1],pe[1]],'g-')
-            a.plot([pcur[0], pcur[0]+100*dxy_local[0]], [pcur[1],pcur[1]+100*dxy_local[1]],'b-')
+#            a.plot([pcur[0], pcur[0]+100*dxy_local[0]], [pcur[1],pcur[1]+100*dxy_local[1]],'b-')
 
     def getG(f0,f1):
         return np.dot(inv(f0.wGc), f1.wGc)
@@ -326,21 +378,21 @@ if __name__ == "__main__":
         Range_r = Baseline*np.sin(phi1)/np.sin(c)     # Edge B = Edge C/sin(c)*sin(b)
         depth_r = ray0[2]*Range_r
         return depth_r
-
+#%%
     ''' set up matching Frame'''
-    refid = 4
+    refid = 3
     fs = []
-    seq = [refid, 0]
+    seq = [refid, 4]
     for fid in seq:
-        f = Frame(frames[fid], wGc[fid], Z=Zs[fid])
+        f = Frame(frames[fid], wGc[fid])#, Z=Zs[fid]
         fs.append(f)
     f0,f1 = fs[0],fs[1]
-    match = f0.searchEPL(f1,4)
-    d = triangulate(f0.px,f0.py,match[0],match[1], getG(f0,f1))
-    plotxyzrgb(f0.makePC(d, 0, 5))
+#    match = f0.searchEPL(f1,4)
+#    d = triangulate(f0.px,f0.py,match[0],match[1], getG(f0,f1))
+#    plotxyzrgb(f0.makePC(d, 0, 5))
 
     ''' plot all image'''
-    if 1:
+    if 0:
         fig = plt.figure(num='all image'); fig.clear()
         gs = plt.GridSpec(len(seq)-1,2)
         a =  fig.add_subplot(gs[:,0])
@@ -356,8 +408,8 @@ if __name__ == "__main__":
             fcur = fs[i+1]
             sp.imshow(fcur.im, interpolation='none')
             sp.plot(fcur.px, fcur.py, 'b.', ms=2)
-            pref = fref.ProjTo(fcur)
-            sp.plot(pref[0], pref[1], 'r.', ms=2)
+#            pref = fref.ProjTo(fcur)
+#            sp.plot(pref[0], pref[1], 'r.', ms=2)
         fig.tight_layout()
 
 
@@ -379,30 +431,10 @@ if __name__ == "__main__":
         cur_samples = sample(f1.im, cur_pos[0], cur_pos[1])
 
         err = np.full(sam_cnt,np.inf,'f')
-        if 0:
-            for i in xrange(sam_cnt):
-                err[i] = np.sum(np.minimum(ref_samples - cur_samples[i:i+2*win_width+1], 1)**2)
-            best = np.nanargmin(err)
-        else:
-            code = r'''
-            int N = (int)sam_cnt;
-            for(int i=0; i<N; i++ )
-            {
-                float diff = 0;
-                for(size_t j=0; j<2*(size_t)win_width+1;j++ )
-                    diff += std::fabs( REF_SAMPLES1(j) - CUR_SAMPLES1(i+j));
-                ERR1(i) = diff;
-            }
-//            std::vector<float> vec(err, err+N);
-//            int winner = vec.begin() - std::min_element(vec.begin(), vec.end());
-//            return_val = Py::new_reference_to(Py::Int(winner));
-            '''
-            weave.inline(code,
-               ['ref_samples','cur_samples','err','sam_cnt','win_width'],
-                compiler='gcc',headers=['<algorithm>','<cmath>','<vector>'],
-                extra_compile_args=['-std=gnu++11 -msse2 -O3'],
-                verbose=2  )
-            best = np.nanargmin(err)
+
+        for i in xrange(sam_cnt):
+            err[i] = np.sum(np.minimum(ref_samples - cur_samples[i:i+2*win_width+1], 1)**2)
+        best = np.nanargmin(err)
 
         if debug:
             f = plt.figure(num='epl match')
@@ -411,8 +443,8 @@ if __name__ == "__main__":
             ab = f.add_subplot(gs[1,:])
 
             al.imshow(f0.im, interpolation='none'); ar.imshow(f1.im, interpolation='none')
-            pt = trueProj(ref_pos[0],ref_pos[1], cGr=inv(rGc), Zr=f0.Z)
-            ar.plot(pt[0], pt[1],'b.')
+#            pt = trueProj(ref_pos[0],ref_pos[1], cGr=inv(rGc), Zr=f0.Z)
+#            ar.plot(pt[0], pt[1],'b.')
             al.plot(ref_pos[0], ref_pos[1],'g.'); al.plot(xr, yr,'r.')
             ar.plot(cur_pos[0], cur_pos[1],'g.')
             ab.plot(err)
@@ -430,7 +462,7 @@ if __name__ == "__main__":
 
         pref = plt.ginput(1, timeout=-1)[0]
         best0 = eplMatch(pref[0], pref[1], f0, f1, win_width=3, debug=True)
+    test_EPLMatch()
 
-
-
+#    test_calcEpl()
 
