174f0a96a23566276d52d57f0ac9b15c61333bfd
Kaihong Huang <kaihong.huang11@gmail.com>
2016-09-04 21:04:42 +0200

    [!]add ground truth comparision

--- dp.py
+++ dp.py
@@ -24,13 +24,11 @@ def sample(dIc,x,y):
     x,y = np.atleast_1d(x, y)
     return scipy.ndimage.map_coordinates(dIc, (y,x), order=1, mode =  'nearest')
 
-#frames, wGc, K, Zs = loaddata1()
-frames, wGc, K = loaddata2()
+frames, wGc, K0, Zs = loaddata1()
+#frames, wGc, K = loaddata2()
 #    from orb_kfs import loaddata4
 #    frames, wGc, K = loaddata4(20)
 #    EpilineDrawer(frames[0:], wGc[0:], K)
-h,w = frames[0].shape[:2]
-fx,fy,cx,cy = K[0,0],K[1,1],K[0,2],K[1,2]
 
 ''' set up matching Frame'''
 fs = []
@@ -41,42 +39,11 @@ for fid in range(len(frames)):
         f = Frame(frames[fid], wGc[fid])
     fs.append(f)
 f0 = fs[0]
-f0.extractPts(K)
 
 if 0:
     baseline = lambda f0,f1: np.linalg.norm(getG(f0, f1)[:3,3])
     fs.sort(key=lambda f: baseline(f0,f))
     [baseline(f0,f) for f in fs]
-
-#%%
-pf()
-pis(f0.im)
-x,y = plt.ginput(0,-1)[0]
-
-def interceptLine(xmin,xmax,ymin,ymax, x0, y0,dxy):
-    tx = (vec(xmin,xmax) - x0) / dxy[0]                # x0+tx*dx = xmin/xmax, (2x1 - 1xN)/1xN
-    tx = np.where(dxy[0]>0, tx, np.roll(tx,1,axis=0))   # tx[0,1] := [v_xmin,v_xmax]
-    ty = (vec(ymin,ymax) - y0) / dxy[1]
-    ty = np.where(dxy[1]>0, ty, np.roll(ty,1,axis=0))   # ty[0,1] := [v_ymin,v_ymax]\
-    v_xmin,v_xmax,v_ymin,v_ymax = tx[0],tx[1],ty[0],ty[1]
-    vmax = np.minimum(v_xmax, v_ymax)
-    vmin = np.maximum(v_xmin, v_ymin)
-    valid_mask = conditions(v_xmin<v_ymax, v_xmax>v_ymin, vmax>0)
-    return vmin, vmax, valid_mask
-
-f1 = fs[2]
-ec = EpilineCalculator(x, y, getG(f0,f1), K)
-dxy_l, dxy,pinf = ec.dxy_local, ec.dxy, ec.nPinf
-
-vri,vra, valid = interceptLine(1,w-1,1,h-1, x, y, dxy_l)
-assert(valid)
-ref = np.fliplr([np.arange(vri,vra+1,dtype='i' )])[0]
-ref_sample = sample(f0.im, x+ref*dxy_l[0], y+ref*dxy_l[1])
-
-vci,vca, valid = interceptLine(1,w-1,1,h-1, pinf[0], pinf[1], dxy)
-assert(valid)
-cur = np.arange(vci, vca+1, dtype='i' )
-cur_sample = sample(f1.im, pinf[0]+cur*dxy[0], pinf[1]+cur*dxy[1])
 
 @timing
 def fast_dp2(ref_sample, cur_sample, occ_cost):
@@ -105,8 +72,13 @@ def fast_dp2(ref_sample, cur_sample, occ_cost):
             for(size_t n=1,nd=0; n<=N; n++,nd++ )
             {
                 float err = REF_SAMPLE1(md) - CUR_SAMPLE1(nd);
-
                 float Edata = err*err;
+                /* The state S[l,r] means there are l pixels in the
+                   left image matched to r pixels in the right. There are 3 possible
+                   ways to reach the state S[l,r]:
+                     a. start from state S[l-1, r-1], match one more pair;
+                     b. start from state S[l-1, r], skip a point in left image l+1->l;
+                     c. start from state S[l, r-1], skip a point in right image r+1->r; */
                 float c1 = C(m-1, n-1) + Edata;
                 float c2 = C(m-1, n) + occ_cost;
                 float c3 = C(m, n-1) + occ_cost;
@@ -149,6 +121,51 @@ def fast_dp2(ref_sample, cur_sample, occ_cost):
                 extra_compile_args=['-std=gnu++11 -msse2 -O3'],
                 verbose=2  )
     return result
+
+def interceptLine(xmin,xmax,ymin,ymax, x0, y0, dxy):
+    # xmin,xmax,ymin,ymax, x0, y0, dxy = 1,w-1,1,h-1, x, y, dxy_l
+    tx = (vec(xmin,xmax) - x0) / dxy[0]                # x0+tx*dx = xmin/xmax, (2x1 - 1xN)/1xN
+    tx = np.where(dxy[0]>0, tx, np.roll(tx,1,axis=0))   # tx[0,1] := [v_xmin,v_xmax]
+    ty = (vec(ymin,ymax) - y0) / dxy[1]
+    ty = np.where(dxy[1]>0, ty, np.roll(ty,1,axis=0))   # ty[0,1] := [v_ymin,v_ymax]\
+    v_xmin,v_xmax,v_ymin,v_ymax = tx[0],tx[1],ty[0],ty[1]
+    vmax = np.minimum(v_xmax, v_ymax)
+    vmin = np.maximum(v_xmin, v_ymin)
+    valid_mask = conditions(v_xmin<v_ymax, v_xmax>v_ymin, vmax>0)
+    return vmin, vmax, valid_mask
+
+#%%
+pyr_level = 0
+factor = 0.5**pyr_level
+fac_mat = np.diag([factor, factor, 1])
+K = fac_mat.dot(K0)
+h,w = f0.pyr_im(pyr_level).shape
+
+f0.extractPts(K, pyr_level=pyr_level)
+
+pf()
+pis(f0.pyr_im(pyr_level))
+x,y = plt.ginput(0,-1)[0]
+
+
+
+f1 = fs[9]
+ec = EpilineCalculator(x, y, getG(f0,f1), K)
+dxy_l, dxy,pinf = ec.dxy_local, ec.dxy, ec.nPinf
+
+vri,vra, valid = interceptLine(1,w-1,1,h-1, x, y, dxy_l)
+assert(valid)
+ref = np.fliplr([np.arange(vri,vra+1,dtype='i')])[0]
+ref_p = (x+ref*dxy_l[0], y+ref*dxy_l[1])
+ref_sample = sample(f0.pyr_im(pyr_level), ref_p[0],ref_p[1])
+
+vci,vca, valid = interceptLine(1,w-1,1,h-1, pinf[0], pinf[1], dxy)
+assert(valid)
+cur = np.arange(vci, vca+1, dtype='i')
+cur_p = (pinf[0]+cur*dxy[0], pinf[1]+cur*dxy[1])
+cur_sample = sample(f1.pyr_im(pyr_level), cur_p[0], cur_p[1])
+
+
 match= fast_dp2(ref_sample, cur_sample, 0.0016)
 
 f = plt.figure()
@@ -157,14 +174,14 @@ a1,a2 = f.add_subplot(gs[0,0]),f.add_subplot(gs[0,1])
 a3 = f.add_subplot(gs[1,:])
 #plt.tight_layout()
 
-a1.imshow(f0.im, interpolation='none', aspect=1)
+a1.imshow(f0.pyr_im(pyr_level), interpolation='none', aspect=1)
 a1.set_title('pick a point in this image')
 a1.autoscale(False)
 a1.plot(x,y,'r.')
 a1.plot([x+vri*dxy_l[0],x+vra*dxy_l[0]],
         [y+vri*dxy_l[1],y+vra*dxy_l[1]])
 
-i2 = a2.imshow(f1.im, interpolation='none', aspect=1)
+i2 = a2.imshow(f1.pyr_im(pyr_level), interpolation='none', aspect=1)
 a2.autoscale(False)
 a2.plot(pinf[0],pinf[1],'r.')
 a2.plot([pinf[0]+vci*dxy[0], pinf[0]+vca*dxy[0]],
@@ -176,3 +193,14 @@ a3.plot(cur, cur_sample, 'b')
 d = ref[mask]+cur[match[mask]]
 
 a1.plot(f0.px,f0.py,'b.',ms=1)
+
+#%%
+ed = EpilineCalculator(ref_p[0][mask], ref_p[1][mask], getG(f0,f1), K)
+z=sample(f0.Z, ref_p[0][mask],ref_p[1][mask])
+p0 = backproject(ref_p[0][mask],ref_p[1][mask],K)
+pz = p0*z
+
+ze = 1.0/ed.DfromV(ref[mask]+cur[match[mask]])
+pe = p0*ze
+plotxyz(pz.T)
+plotxyz(pe.T,hold=True)
